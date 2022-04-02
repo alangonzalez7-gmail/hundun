305c089107bc5fb9d31113dd7740f2b43618cef6
Alan González <alangonzalez7@hotmail.com>
2022-04-02 11:08:03 -0600

    Fixed item handler logic

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import { React, useContext } from 'react';
+import React, {useContext} from 'react';
 import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
